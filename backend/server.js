13c5ba1ef90aafe28a5d435613bc2dabf825fde4
Abhinav Kumar <ak.kr.9852@gmail.com>
2026-07-08 11:17:04 +0530

    Fix Render deployment issue

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,17 @@
 require("dotenv").config();
-console.log("MONGO_URI =", process.env.MONGO_URI);
 
 const app = require("./app");
 const connectDB = require("./config/db");
-const listEndpoints = require("express-list-endpoints");
 
 const PORT = process.env.PORT || 5000;
 
 // Connect Database
 connectDB();
 
-// Print all registered routes
-console.log("===== REGISTERED ROUTES =====");
-console.log(listEndpoints(app));
-console.log("=============================");
-
 // Start Server
 app.listen(PORT, () => {
     console.log("==================================");
-    console.log(`🚀 CitizenConnect Server Running`);
-    console.log(`🌐 http://localhost:${PORT}`);
+    console.log("🚀 CitizenConnect Server Running");
+    console.log(`🌐 Server running on port ${PORT}`);
     console.log("==================================");
 });
