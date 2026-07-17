8018f554ed197d300e4f5287730b7a783fa58a87
Abhinav Kumar <ak.kr.9852@gmail.com>
2026-07-17 11:28:59 +0530

    Fix Api configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,22 +1,27 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://citizenconnect-nm4v.onrender.com/api",
+
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
-});
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 api.interceptors.response.use(
   (response) => response,
